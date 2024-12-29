72b01a046b0d504c560947b502132029e834d3f2
Bhargav Chickmagalur Nanjundappa <chickmagalurnanjun.b@northeastern.edu>
2024-12-28 21:51:20 -0500

    Seperate errors

--- qec_builder.py
+++ qec_builder.py
@@ -1,28 +1,30 @@
 from stim_circuit import Circuit
 class QECBuilder:
-  def __init__(self, distance=5, error_rate=0.001):
+  def __init__(self, distance=5, repeat=5,  x_error=0.001, depolarize_error=0.001):
     assert distance > 0
     self.distance = (distance*2)-1
-    self.error_rate = error_rate
+    self.x_error = x_error
+    self.depolarize_error = depolarize_error
+    self.repeat = repeat
 
 
   
   def build_cx_layer(self):
     cx_qubits = list(range(self.distance))[:-1]
     self.circuit.cx(cx_qubits)
-    self.circuit.depolarize(self.error_rate, cx_qubits)
-    self.circuit.depolarize(self.error_rate, [self.distance-1], single_qubit=True) # 1q depolarize
+    self.circuit.depolarize(self.depolarize_error, cx_qubits)
+    self.circuit.depolarize(self.depolarize_error, [self.distance-1], single_qubit=True) # 1q depolarize
     self.circuit.tick()
 
 
     cx_qubits = [x for i in range(2, self.distance, 2) for x in (i, i-1)]
     self.circuit.cx(cx_qubits)
-    self.circuit.depolarize(self.error_rate, cx_qubits)
-    self.circuit.depolarize(self.error_rate, [0], single_qubit=True)
+    self.circuit.depolarize(self.depolarize_error, cx_qubits)
+    self.circuit.depolarize(self.depolarize_error, [0], single_qubit=True)
     self.circuit.tick()
   
   def measure_non_data(self, non_data_qubits):
-    self.circuit.x_error(self.error_rate, non_data_qubits)
+    self.circuit.x_error(self.x_error, non_data_qubits)
     self.circuit.measure(non_data_qubits)
 
   def __seperate_qubits(self):
@@ -36,13 +38,13 @@ class QECBuilder:
 
   def __initalize_build(self):
     self.circuit.reset(range(self.distance))
-    self.circuit.x_error(self.error_rate, range(self.distance))
+    self.circuit.x_error(self.x_error, range(self.distance))
     self.circuit.tick()
   
   def __build_core_layer(self):
     self.build_cx_layer()
     self.measure_non_data(self.non_data_qubits)
-    self.circuit.depolarize(self.error_rate, self.data_qubits, single_qubit=True)
+    self.circuit.depolarize(self.depolarize_error, self.data_qubits, single_qubit=True)
   
   def __add_single_detectors(self):
     total_non_data_qubits = len(self.non_data_qubits)
@@ -52,14 +54,14 @@ class QECBuilder:
   def __build_repitition_layer(self):
       total_non_data_qubits = len(self.non_data_qubits)
       self.circuit.reset(self.data_qubits)
-      self.circuit.x_error(self.error_rate, self.data_qubits)
+      self.circuit.x_error(self.depolarize_error, self.data_qubits)
       self.__build_core_layer()
       self.circuit.shift_coords(0,1)
       for i,q in enumerate(self.non_data_qubits):
         self.circuit.detector(q,0,-((total_non_data_qubits)-i), [-((2*total_non_data_qubits)-i)])
 
   def __build_final_layer(self):
-      self.circuit.x_error(self.error_rate, self.data_qubits)
+      self.circuit.x_error(self.x_error, self.data_qubits)
       self.circuit.measure(self.data_qubits)
       self.circuit.shift_coords(0,1)
 
@@ -80,7 +82,7 @@ class QECBuilder:
     self.circuit:Circuit = Circuit()
     self.__seperate_qubits()
     self.__build_initial_layer()
-    self.circuit.repeat(5, self.__build_repitition_layer)
+    self.circuit.repeat(self.repeat, self.__build_repitition_layer)
     self.__build_final_layer()
   
   def generate(self):
